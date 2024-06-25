be8d76fd45ed979befc3a9b38e633b4e8fc1dfc4
ryo-ha <80822677+ryo-ha-gi@users.noreply.github.com>
2024-06-25 13:45:38 +0000

    Add: 万有引力

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,9 @@ export default function Home() {
   useEffect(() => {
     const engine = Matter.Engine.create();
     const world = engine.world;
+    const canvasElement = document.getElementById("matter-scene") || document.body;
     const render = Matter.Render.create({
-      element: document.body,
+      element: canvasElement,
       engine: engine,
       options: {
         width: 800,
@@ -23,6 +24,8 @@ export default function Home() {
         wireframes: false,
       },
     });
+
+    engine.gravity.y = 0;
 
     const ground = Matter.Bodies.rectangle(400, 580, 810, 60, { isStatic: true });
     Matter.World.add(world, ground);
@@ -51,6 +54,12 @@ export default function Home() {
       setBoxes(currentBoxes =>
         currentBoxes.map(box => {
           const boxBody = boxBodies.find(b => b.id === box.id);
+          const center = { x: 400, y: 300 };
+          const delta = boxBody? { x: boxBody.body.position.x - center.x, y: boxBody.body.position.y - center.y } : { x: 0, y: 0 };
+          const force_distance = boxBody? Math.max(Math.sqrt( delta.x**2 + delta.y**2 ),10): Infinity;
+          const magnitude = -1;
+          const force = boxBody? { x: magnitude * delta.x / (force_distance**3), y: magnitude * delta.y / (force_distance**3),} :{ x: 0, y: 0 };
+          if (boxBody) Matter.Body.applyForce(boxBody.body, center, force);
           return boxBody
             ? { ...box, x: boxBody.body.position.x, y: boxBody.body.position.y }
             : box;
@@ -85,11 +94,12 @@ export default function Home() {
             top: box.y - 40, // 中心を基準に位置を調整
             width: 80,
             height: 80,
-            backgroundColor: 'red',
+            backgroundColor: "rgba(255, 255, 128, .5)",
+            "pointerEvents":"none"
           }}
         />
       ))}
-      <div id="matter-scene" />
+      <div id="matter-scene" onPointerMove={e => setMouse_coordninates({x:e.clientX,y:e.clientY})}/>
       <p>x:{mouse_coordinates.x} y:{mouse_coordinates.y}</p>
     </div>
   );
