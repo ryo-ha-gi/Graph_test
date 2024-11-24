4cb2e5e7a48806a2fcfad53e9aefa42c5dcba137
ryo-ha <80822677+ryo-ha-gi@users.noreply.github.com>
2024-11-24 13:07:51 +0000

    Fix: page to dynamically load SigmaGraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 "use client"
-import SigmaGraph from './sigmagraph';
+import dynamic from "next/dynamic";
+
+// SigmaGraph を動的にインポート（SSR無効化）
+const SigmaGraph = dynamic(() => import("./sigmagraph"), { ssr: false });
+
 export default function Home() {
 
   return (
